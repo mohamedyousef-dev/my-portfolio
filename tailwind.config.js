f2b9f06d71c5738000ef33e88ca4bf0a30b2a19d
Mohamed Yousef <mohamed.yoosef.official@gmail.com>
2026-06-04 05:27:41 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,22 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: '#00f2fe',
+          dark: '#052c30',    
+          light: '#7fecff',   
+        },
       },
+      boxShadow: {
+        'neon': '0 0 5px #00f2fe, 0 0 20px rgba(0, 242, 254, 0.6)',
+        'neon-strong': '0 0 10px #00f2fe, 0 0 40px #00f2fe',
+      }
     },
   },
   plugins: [],
-};
+}
